60bb0f07947f76b82eb0da16e9330d508160e547
geoff024 <geoff024@kali-024.geoff-024>
2025-01-30 13:47:02 +0300

    Fix ESLint and type errors for deployment

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+import type { ThemeProvider as NextThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
