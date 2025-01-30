43e2f7c796809213703f4ce3f58e3fdc10f17899
geoff024 <geoff024@kali-024.geoff-024>
2025-01-30 13:40:11 +0300

    Fix ESLint and type errors for deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
-const { FlatCompat } = require("@eslint/eslintrc")
-const path = require("path")
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
 module.exports = {
-  extends: ["next/core-web-vitals", "next/typescript"],
-  // You can add more custom rules or overrides here if needed
+  extends: ["next"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 }
-
 
